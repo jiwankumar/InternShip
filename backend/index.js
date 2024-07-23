12adc007b7b93bbf8b27a4f03b47d1c5008464bb
jiwankumar <jiwanjhumba@gmail.com>
2024-07-23 11:06:31 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express =require("express")
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const router = require("./routes/userr")
+const cookieParser = require("cookie-parser")
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:["http://localhost:3001"],
+    credentials:true
+}))
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to database")
+})
+app.use("/auth",router)
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
+
+
+
